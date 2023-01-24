312b2f818598659a04d837ffd5eac0458f19f217
Tanvir Irfan Chowdhury <tanvirirfan.utsa@gmail.com>
2023-01-24 17:52:13 -0500

    adding more agent parameters

--- Assets/AgentParameterGeneration.cs
+++ Assets/AgentParameterGeneration.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AgentParameterGeneration : MonoBehaviour 
-{
-    private enum MovementPercentChange {
+public class AgentParameterGeneration : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject spawnPoint;
+    private AgentParameterChances agentParameterChances;
+
+    public enum MovementPercentChange {
 
         None = 0,
         WakingStick = 15,
@@ -12,26 +16,61 @@ public class AgentParameterGeneration : MonoBehaviour
         WheelChair = 10
 
     }
-    private enum Gender {
-        Male, 
+    public enum Gender {
+        Male,
         Female
     }
 
-    
+    public enum SpatialKnowledge {
+        Low, 
+        Medium, 
+        High
+    }
+
+    public enum EmergencyRecognition {
+        Immediate, 
+        Lagging
+    }
+    private void Awake() {
+
+        agentParameterChances = gameObject.GetComponent<AgentParameterChances>();
+    }
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
     public void GenerateAgents() {
+        int agentPop = 50;
+        GameObject agentPrefab = Resources.Load<GameObject>("Agent");
+
+        for (int i = 0; i < agentPop; i++) {
+            GameObject agent = Instantiate(agentPrefab, null, spawnPoint.transform);
+            agent.transform.name = "Agent" + i;
+            AgentParameters agentParam = agent.AddComponent<AgentParameters>();
+            agentParam.Gender = calcGender();
+
+
+
+        }
+
 
     }
-    
+
+    private Gender calcGender() {
+
+        float generateRandomFloat = Random.Range(1f, 99f);
+        if(generateRandomFloat < agentParameterChances.malePercentage) {
+            return Gender.Male;
+        } else {
+            return Gender.Female;
+        }
+
+    }
+
 }
--- Assets/AgentParameters.cs
+++ Assets/AgentParameters.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Parameters : MonoBehaviour
+public class AgentParameters : MonoBehaviour
 {
+    [Header("Agent Parameters")]
+    private AgentParameterGeneration.Gender gender;
+    private AgentParameterGeneration.MovementPercentChange movementPercentChange;
+    private AgentParameterGeneration.SpatialKnowledge spatialKnowledge;
+    private float speed;
+    private float stress;
+    private float nervousness;
+    public AgentParameterGeneration.Gender Gender { get => gender; set => gender = value; }
+    public AgentParameterGeneration.MovementPercentChange MovementPercentChange { get => movementPercentChange; set => movementPercentChange = value; }
+    public AgentParameterGeneration.SpatialKnowledge SpatialKnowledge { get => spatialKnowledge; set => spatialKnowledge = value; }
+    public float Speed { get => speed; set => speed = value; }
+    public float Stress { get => stress; set => stress = value; }
+    public float Nervousness { get => nervousness; set => nervousness = value; }
+
+
+    private NavMeshAgent navMeshAgent;
+    private pathfinding pathfinding;
+    public Material maleMaterial;
+    public Material femaleMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pathfinding = gameObject.GetComponent<pathfinding>();
+        pathfinding.exit = GameObject.FindGameObjectWithTag("exit");
+
+        maleMaterial = Resources.Load<Material>("Male");
+        femaleMaterial = Resources.Load<Material>("Female");
+        if (gender == AgentParameterGeneration.Gender.Male) {
+            GetComponent<Renderer>().material = maleMaterial;
+        } else {
+            GetComponent<Renderer>().material = femaleMaterial;
+        }
+
+  
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
     }
+
+
 }
